1080990c7e94c741a60df87646763bf2120701d4
Stefan Jung <stefan@theappbusiness.com>
2018-10-12 12:07:35 +0200

    implemented UE BAS2

--- org.dieschnittstelle.jee.esa.bas/src/main/java/org/dieschnittstelle/jee/esa/basics/ShowAnnotations.java
+++ org.dieschnittstelle.jee.esa.bas/src/main/java/org/dieschnittstelle/jee/esa/basics/ShowAnnotations.java
@@ -4,33 +4,68 @@ package org.dieschnittstelle.jee.esa.basics;
 import org.dieschnittstelle.jee.esa.basics.annotations.AnnotatedStockItemBuilder;
 import org.dieschnittstelle.jee.esa.basics.annotations.StockItemProxyImpl;
 
+import javax.xml.stream.events.Attribute;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import static org.dieschnittstelle.jee.esa.utils.Utils.*;
 
 public class ShowAnnotations {
 
-	public static void main(String[] args) {
-		// we initialise the collection
-		StockItemCollection collection = new StockItemCollection(
-				"stockitems_annotations.xml", new AnnotatedStockItemBuilder());
-		// we load the contents into the collection
-		collection.load();
+    public static void main(String[] args) {
+        // we initialise the collection
+        StockItemCollection collection = new StockItemCollection(
+                "stockitems_annotations.xml", new AnnotatedStockItemBuilder());
+        // we load the contents into the collection
+        collection.load();
 
-		for (IStockItem consumable : collection.getStockItems()) {
-			;
-			showAttributes(((StockItemProxyImpl)consumable).getProxiedObject());
-		}
+        for (IStockItem consumable : collection.getStockItems()) {
+            ;
+            showAttributes(((StockItemProxyImpl) consumable).getProxiedObject());
+        }
 
-		// we initialise a consumer
-		Consumer consumer = new Consumer();
-		// ... and let them consume
-		consumer.doShopping(collection.getStockItems());
-	}
+        // we initialise a consumer
+        Consumer consumer = new Consumer();
+        // ... and let them consume
+        consumer.doShopping(collection.getStockItems());
+    }
 
-	/*
-	 * UE BAS2 
-	 */
-	private static void showAttributes(Object consumable) {
-		show("class is: " + consumable.getClass());
-	}
+    /*
+     * UE BAS2
+     */
+    private static void showAttributes(Object consumable) {
+        try {
+
+            Class klass = consumable.getClass();
+
+            StringBuilder output = new StringBuilder("{" + klass.getSimpleName() + ": ");
+
+            List<Method> getterMethod = Arrays.stream(klass.getDeclaredMethods())
+                    .filter(x -> x.getName().startsWith("get")).collect(Collectors.toList());
+
+            int i = 0;
+            for (Method getter : getterMethod) {
+                i++;
+
+                String[] split = getter.getName().split("get");
+                output.append(split[1].toLowerCase()).append(":").append(getter.invoke(consumable));
+                if (i != getterMethod.size()) {
+                    output.append(", ");
+                }
+            }
+
+            output.append("}");
+            show(output);
+
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
